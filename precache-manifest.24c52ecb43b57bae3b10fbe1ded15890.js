d3ff73131c73b2f8c56f617002f004f92a8baeb3
ANKIT RAJ <ankitraj4206@gmail.com>
2022-09-04 00:29:45 +0530

    Updates

---
+++ precache-manifest.24c52ecb43b57bae3b10fbe1ded15890.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdc86b5968fb9ccc48c67fb1fdb5b8f",
+    "url": "/Text-Notes/index.html"
+  },
+  {
+    "revision": "e3ff99a031cbec351aa1",
+    "url": "/Text-Notes/static/js/2.ff8ac841.chunk.js"
+  },
+  {
+    "revision": "193f9073da02328fd025",
+    "url": "/Text-Notes/static/js/main.e6539d14.chunk.js"
+  },
+  {
+    "revision": "507e772f399e16cf151d",
+    "url": "/Text-Notes/static/js/runtime-main.d63c5cf9.js"
+  }
+]);
